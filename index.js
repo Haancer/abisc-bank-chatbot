e64fb3f286725e2a9bdeb99b02a462b0aa32175c
Haancer <kenhancer@aol.com>
2026-06-07 00:54:55 +0000

    Fix static files path for Render

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/chat", chatRoute);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-}); 
+  res.sendFile(path.join(__dirname, "Public", "index.html"));
+});
 
 app.listen(3000, () => {
   console.log("ABISC Bank chatbot running on http://localhost:3000");
