2ec89ffe15b95b32b9cbdbc7bcde39c983e6a544
Haancer <kenhancer@aol.com>
2026-06-07 01:04:05 +0000

    Fix public folder casing

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/chat", chatRoute);
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "Public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(3000, () => {
