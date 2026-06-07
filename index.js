347f3428c93e7987ad09bd899f7316fac846fbfc
Haancer <kenhancer@aol.com>
2026-06-07 00:40:03 +0000

    Add vercel config and path fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const path = require("path");
 
 const chatRoute = require("./routes/chat");
 
@@ -11,6 +12,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.use("/chat", chatRoute);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+}); 
 
 app.listen(3000, () => {
   console.log("ABISC Bank chatbot running on http://localhost:3000");
